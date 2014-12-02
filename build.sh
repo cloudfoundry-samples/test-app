31b964a8af6facb394c9f06be36ff1ca7331b89e
Onsi Fakhouri <onsijoe@gmail.com>
2014-12-02 10:11:10 -0800

    work with either instance index environment variable

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ GOOS=linux GOARCH=amd64 go build .
 echo "Constructing Dockerimage"
 docker build -t="cloudfoundry/lattice-app" .
 docker push cloudfoundry/lattice-app
+
+echo "Cleaning up..."
+rm lattice-app
--- helpers/fetch_index.go
+++ helpers/fetch_index.go
@@ -7,5 +7,8 @@ import (
 
 func FetchIndex() (int, error) {
 	index := os.Getenv("CF_INSTANCE_INDEX")
+	if index == "" {
+		index = os.Getenv("INSTANCE_INDEX")
+	}
 	return strconv.Atoi(index)
 }
